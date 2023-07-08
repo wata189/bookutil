d8b1afc05a882e6e94669c4993d8fd877bf69ebb
wata189 <watarusato105@gmail.com>
2023-07-08 12:43:03 +0900

    #25 SELECT以外のSQL処理追加

--- lambda/modules/mysql_util.py
+++ lambda/modules/mysql_util.py
@@ -12,9 +12,30 @@ class My_Sql:
     def __init__(self, cursor) -> None:
         self.cursor = cursor
 
-    def select(self, sql, params=[]):
+    # SQL実行の共通処理
+    def execute(self, sql, params=[]):
         self.cursor.execute(sql, params)
+
+    # 取得
+    def select(self, sql, params=[]):
+        self.execute(sql, params)
         return self.cursor.fetchall()
+    
+    # 追加
+    def insert(self, sql, values):
+        self.cursor.executemany(sql, values)
+    
+    # 更新
+    def update(self, sql, params=[]):
+        self.execute(sql, params)
+    
+    # 論理削除
+    def delete_logically(self, sql, params=[]):
+        self.update(sql, params)
+    
+    # 物理削除
+    def delete_physically(self, sql, params=[]):
+        self.execute(sql, params)
         
         
 
