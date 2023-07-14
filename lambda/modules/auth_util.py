cb96237a8951418fc23bde79a7e0b5b215fe4a82
wata189 <watarusato105@gmail.com>
2023-07-14 13:43:14 +0900

    #69　ローカル環境の認証処理修正

--- lambda/modules/auth_util.py
+++ lambda/modules/auth_util.py
@@ -1,6 +1,9 @@
 import jwt
 import os
 
+ENV = os.environ['ENV']
+USE_AUTH = True if os.environ['USE_AUTH'] == "True" else False 
+IS_AUTH = True if os.environ['IS_AUTH'] == "True" else False 
 region = os.environ['AUTH_REGION']
 user_pool_id = os.environ['AUTH_USER_POOL_ID']
 client_id = os.environ['AUTH_CRIENT_ID']
@@ -10,6 +13,11 @@ jwks_url = f'{issuer}/.well-known/jwks.json'
 
 # トークンが使えるか確認する処理
 def is_auth(token:str):
+
+    # 開発環境の場合は環境変数を見る
+    if ENV == "dev" and not USE_AUTH:
+        return IS_AUTH
+
     try:
         jwks_client = jwt.PyJWKClient(jwks_url)
         signing_key = jwks_client.get_signing_key_from_jwt(token)
