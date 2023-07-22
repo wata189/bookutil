b36723514c6f14153220ee3a5201559f95d7afb9
wata189 <watarusato105@gmail.com>
2023-07-22 18:27:41 +0900

    #51 リフレッシュトークン処理追加

--- src/modules/authUtil.ts
+++ src/modules/authUtil.ts
@@ -4,18 +4,22 @@ import axios from 'axios';
 const authUrl = import.meta.env.VITE_AUTH_URL;
 const clientId = import.meta.env.VITE_AUTH_CRIENT_ID;
 const redirectUrl = encodeURI(import.meta.env.VITE_APP_URL);
+
+const URL_OAUTH2_TOKEN = `${authUrl}/oauth2/token`;
+const URL_OAUTH2_USERINFO = `${authUrl}/oauth2/userInfo`;
+
 type User = {
   email:string
 }
 type Tokens = {
-  idToken: string,
-  accessToken: string
+  accessToken: string,
+  refreshToken: string
 }
 // トークン取得処理
 const getToken = async (code: string):Promise<Tokens> =>{
   const tokens = {
-    'idToken': '',
-    'accessToken': ''
+    'accessToken': '',
+    "refreshToken": ""
   };
   try{
     // code→アクセストークンを取得
@@ -25,19 +29,18 @@ const getToken = async (code: string):Promise<Tokens> =>{
       'redirect_uri': redirectUrl,
       'code': code
     };
-    const url = `${authUrl}/oauth2/token`;
-    const response = await axios.post(url, params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}});
+    const response = await axios.post(URL_OAUTH2_TOKEN, params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}});
     if(response){
       const data = response.data;
-      tokens.idToken = data.id_token;
       tokens.accessToken = data.access_token;
+      tokens.refreshToken = data.refresh_token;
     }
   }catch(error){
     console.log(error);
   }finally{
     // トークンをローカルストレージに設定
     localStorage.accessToken = tokens.accessToken;
-    localStorage.idToken = tokens.idToken;
+    localStorage.refreshToken = tokens.refreshToken;
     return tokens;
   }
 } 
@@ -45,21 +48,44 @@ const getToken = async (code: string):Promise<Tokens> =>{
 // アクセストークン→ユーザー情報を取得
 const getUserInfo = async (accessToken: string):Promise<User> =>{
   const user = {email: ''};
-  try{
 
-    const userInfoUrl = `${authUrl}/oauth2/userInfo`;
+  try{
     const userInfoHeaders = {
       'Authorization': `Bearer ${accessToken}`
     };
-    const userResponse = await axios.get(userInfoUrl, {headers: userInfoHeaders});
+    const userResponse = await axios.get(URL_OAUTH2_USERINFO, {headers: userInfoHeaders});
     if(userResponse){
       user.email = userResponse.data.email;
     }
-  
-
-    return user;
   }catch(error){
     console.log(error);
+
+    try{
+      // 1回refresh_token使ってaccess_token取得し直し
+      const refreshToken = localStorage.refreshToken || '';
+      const params = {
+        'grant_type': 'refresh_token',
+        'client_id': clientId,
+        "refresh_token": refreshToken
+      };
+      const response = await axios.post(URL_OAUTH2_TOKEN, params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}});
+      if(response){
+        const data = response.data;
+        localStorage.accessToken = data.access_token;
+  
+        // 取得したaccessTokenを使ってもう一度ユーザー情報取得
+        const userInfoHeaders = {
+          'Authorization': `Bearer ${data.access_token}`
+        };
+        const userResponse = await axios.get(URL_OAUTH2_USERINFO, {headers: userInfoHeaders});
+        if(userResponse){
+          user.email = userResponse.data.email;
+        }
+      }
+    }catch(innerError){
+      console.log(innerError);
+    }
+  }finally{
     return user;
   }
 };
@@ -72,7 +98,7 @@ const login = () => {
 const logout = () => {
   // アクセストークン初期化
   localStorage.accessToken = "";
-  localStorage.idToken = "";
+  localStorage.refreshToken = "";
 
   const url = `${authUrl}/logout?client_id=${clientId}&logout_uri=${redirectUrl}`;
   window.location.href = url;
