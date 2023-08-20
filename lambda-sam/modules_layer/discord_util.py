0f58f165dc365ff2194b6665bac35aa9977faa3d
wata189 <watarusato105@gmail.com>
2023-08-20 14:34:23 +0900

    lambda処理削除

--- lambda-sam/modules_layer/discord_util.py
+++
@@ -1,8 +0,0 @@
-import os
-from discordwebhook import Discord
-
-DISCORD_URL = os.getenv("DISCORD_URL")
-
-def send_discord(msg):
-    discord = Discord(url=DISCORD_URL)
-    discord.post(content=msg)
