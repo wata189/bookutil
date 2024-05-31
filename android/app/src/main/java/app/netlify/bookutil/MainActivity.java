7d3c9cd407fdea1b0fd9c5e8adce302dd143b870
sato wataru <watarusato105@gmail.com>
2024-05-31 13:24:45 +0900

    #246 capacitorの導入

---
+++ android/app/src/main/java/app/netlify/bookutil/MainActivity.java
@@ -0,0 +1,5 @@
+package app.netlify.bookutil;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.netlify.bookutil',
+  appName: 'bookutil',
+  webDir: 'dist'
+};
+
+export default config;
